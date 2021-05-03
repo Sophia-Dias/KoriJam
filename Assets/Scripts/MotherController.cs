badb1a1349c2b0c4fa528b541bc140a11f4db319
Ricardo Godoy <rq.godoy@gmail.com>
2021-05-03 15:35:55 -0300

    Fix no disparo de evento de conclusão do jogo

--- Assets/Scripts/MotherController.cs
+++ Assets/Scripts/MotherController.cs
@@ -7,7 +7,7 @@ public class MotherController : MonoBehaviour
   public DialogObject dialog;
   private void OnTriggerEnter(Collider other)
   {
-    if (other.gameObject.layer != LayerMask.NameToLayer("Player")) return;
+    if (!other.gameObject.CompareTag("Player")) return;
     DialogManager.Instance.OnFinish.AddListener(ShowCredits);
     DialogManager.Instance.StartDialog(dialog);
   }
