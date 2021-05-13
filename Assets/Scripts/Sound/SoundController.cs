28339d29ac5891a71c63f7e9365414957a3d935d
Ricardo Godoy <rq.godoy@gmail.com>
2021-05-13 14:53:45 -0300

    Novo sistema de shader

---
+++ Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+  public float range;
+  public float time = 0;
+
+  private void OnDrawGizmosSelected() {
+    Gizmos.DrawWireSphere(transform.position, range);
+  }
+
+  private void OnEnable() => SoundManager.AddSound(this);
+  private void OnDisable() => SoundManager.RemoveSound(this);
+
+  private void Update() {
+    time += Time.deltaTime * 2;
+  }
+}
