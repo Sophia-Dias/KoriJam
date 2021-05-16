1ad8582c1139ca1cacea98e1aae6ff41c5f36677
Ricardo Godoy <rq.godoy@gmail.com>
2021-05-15 21:39:22 -0300

    Adiciona a classe Repeating Sound

--- Assets/Scripts/Sound/SoundController.cs
+++ Assets/Scripts/Sound/SoundController.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof (AudioSource))]
 public class SoundController : MonoBehaviour
 {
+  [Range(0.01f, 10f)]
   public float range;
   public float time = 0;
+  public AudioSource audioSource;
 
   private void OnDrawGizmosSelected() {
     Gizmos.DrawWireSphere(transform.position, range);
@@ -14,7 +18,18 @@ public class SoundController : MonoBehaviour
   private void OnEnable() => SoundManager.AddSound(this);
   private void OnDisable() => SoundManager.RemoveSound(this);
 
-  private void Update() {
+  private void Start() {
+    audioSource = GetComponent<AudioSource>();
+  }
+
+  protected virtual void Update() {
+    if (!GameManager.Instance.CanMove()) return;
     time += Time.deltaTime * 2;
+  }
+
+  protected void PlaySound()
+  {
+    time = 0;
+    audioSource.Play();
   }
 }
